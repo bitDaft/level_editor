30fa39e670af660ca48b326cb3805d6426e195fb
meow-man <anodecode@gmail.com>
2015-11-17 09:21:32 +0530

    Added menu, bugs present

--- source/main.cpp
+++ source/main.cpp
@@ -1,53 +1,60 @@
 #include "listfurs.h"
 #include "colors.h"
+#include "furs.h"
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
 #include <fstream>
 
-using namespace std;
 
-#define MX    20
-#define MY    10
+using namespace std;
+//unsigned short GRIDSIZE = 10;
+
+// To get grid in ratio of almost square looking it is advised to keep MX=2*MY
+// only change MY ,unless you need specific size grid
+#define MX    (MY * 2)
+#define MY    10//GRIDSIZE
+
 
 void gotoxy(COORD d)
 {
   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), d);
 }
 
-void printgrid()
+void initscreen()
 {
+  unsigned short register i, j;
+
+  lblue;
+  gotoxy({ 0, 0 });
+
+  for (i = 0; i < 80; i++)
+    for (j = 0; j < 25; j++) cout << char(177);
+  white;
+  gotoxy({ 0, 0 });
+}
+
+void printgrid() {
   uint8_t register i, j;
 
-  for (i = 0; i < 10; i++)
-  {
-    for (j = 0; j < 20; j++)
-    {
+  for (i = 0; i < MY; i++) {
+    for (j = 0; j < MX; j++)
       if ((j == 0) || (i == 0)) cout << "+-";
       else cout << " -";
-    }
     cout << "+\n|";
 
-    for (j = 0; j < 20; j++)
-    {
-      cout << " |";
-    }
+    for (j = 0; j < MX; j++) cout << " |";
     cout << "\n";
   }
 
-  for (i = 0; i < 20; i++)
-  {
-    cout << "+-";
-  }
+  for (i = 0; i < MX; i++) cout << "+-";
   cout << "+";
 }
 
-int hitkey()
-{
+int hitkey() {
   int x = 0;
 
-  if (kbhit())
-  {
+  if (kbhit()) {
     x = getch();
 
     if (!x || (x == 224)) x = getch();
@@ -55,134 +62,308 @@ int hitkey()
   return x;
 }
 
-void clearlist(COORD pos, listfurs  *top)
+void clearlist(COORD pos, int line)
 {
-  listfurs *temp = top; gotoxy(pos);
+  gotoxy(pos);
 
-  while (temp)
+  while (line)
   {
-    temp = temp->getp();
-    cout << "                          ";
+    cout << "                            ";
     pos.Y += 1;
     gotoxy(pos);
+    line--;
   }
   cout << "                            ";
 }
 
 void producelist(COORD pos, listfurs  *top)
 {
-  listfurs *temp = top; pos.Y += 1;
+   pos.Y += 1;
 
-  clearlist(pos, top);
-  gotoxy({ pos.X, short(pos.Y - 2) });
+  clearlist(pos, 20);
+  gotoxy({ pos.X, static_cast<short>(pos.Y - 2) });
   cout.width(8); cout << "Gridval ";
   cout.width(10); cout << "fur point ";
   cout.width(10); cout << "fur->next";
   gotoxy({ pos.X, short(pos.Y - 1) });
   cout << "============================";
+  gotoxy({ 1, 1 });
 
-  while (temp)
+  while (top)
   {
     gotoxy(pos);
     cout.width(7);
-    cout << temp->Getgridval() << " ";
+    cout << top->Getgridval() << " ";
     cout.width(9);
-    cout << temp;
+    cout << top;
     cout.width(10);
-    cout << temp->getp();
-    temp   = temp->getp();
+    cout << top->getp();
+    top   = top->getp();
     pos.Y += 1;
   }
 }
 
-// TODO complete menu in pointer list pos (45,0)
-void menu()
+int menu()
 {
-  gotoxy({ 45, 0 });
-  cout << "hi";
+  unsigned int dir = 0;
+  short y { 0 };
+
+  gotoxy({ 45, y++ }); cout << "+=======================+";
+  gotoxy({ 45, y++ }); cout << "|          MENU         |";
+  gotoxy({ 45, y++ }); cout << "+=======================+";
+  gotoxy({ 45, y++ }); cout << "|      NEW LAYOUT       |";
+  gotoxy({ 45, y++ }); cout << "|      OPEN LAYOUT      |";
+  gotoxy({ 45, y++ }); cout << "|      DELETE LAYOUT    |";
+ // gotoxy({ 45, y++ }); cout << "|      GRID SIZE(deprec)|";//deprecated option
+  gotoxy({ 45, y++ }); cout << "|      EXIT             |";
+  gotoxy({ 45, y++ }); cout << "+-----------------------+";
+  y = 3;
+  gotoxy({ 50, y });
+  cout << ">";
+  gotoxy({ 79, 24 });
+
+  while ((dir = hitkey()) != 13) {
+    switch (dir)
+    {
+    case 72:
+
+      if (y > 3) {
+	gotoxy({ 50, y }); cout << " "; y--;
+      }
+      gotoxy({ 50, y });
+      cout << ">"; gotoxy({ 79, 24 }); break;
+
+    case 80:
+
+      if (y < 6) {
+	gotoxy({ 50, y }); cout << " "; y++;
+      }
+      gotoxy({ 50, y });
+      cout << ">"; gotoxy({ 79, 24 }); break;
+    }
+  }
+  y -= 3;
+  return y;
+}
+void drawbool(bool gr[MY][MX])
+{
+  unsigned register int i,j;
+  listfurs *p;
+   furs *op;
+   unsigned short ch;
+  for ( i = 0; i < MY; ++i)
+  {
+    /* code */
+    for ( j = 0; j < MX; ++j)
+    {
+      // cout<<"hi";
+       gotoxy({static_cast<short>(j*2+1),static_cast<short>(i*2+1)});
+
+      if(gr[i][j]==true)
+      {// cout<<"hi";
+        p=&getfur(i*20+j);
+        // cout<<"hi";
+      op=p->getf();
+      // cout<<"hi";
+      ch=op->getcolor();
+        changecolor(ch);
+        gotoxy({static_cast<short>(j*2+1),static_cast<short>(i*2+1)});
+
+        cout<<(char)177;
+        white;
+      }
+      else cout<<" ";
+    }
+  }
 }
 
 // TODO implement features like save edit delete export and view
 int main()
 {
-  bool grid[MY][MX] {};
-  listfurs *ptr = NULL;
+  bool saved     = false;
+  bool bckToMenu = false, quit = false;
+  listfurs *ptr  = NULL;
   unsigned int ch, value = 0;
   COORD        d = { 1, 1 };
+  bool gchanged=false;
 
-  printgrid();
-  gotoxy(d);
-  menu();
-  cin.ignore();
-  producelist({ 45, 1 }, ptr);
+  do {
+    saved=false;
+     initscreen();
 
-  while (1)
-  {
-    if ((ch = hitkey()))
+    Sleep(250);
+    printgrid();
+    bckToMenu = false;
+    int opt;
+bool grid[MY][MX];
+unsigned short cnt;
+unsigned short tcnt;
+//unsigned short val=0;
+ifstream infur;
+
+switch(opt = menu()){
+    case 1:
     {
-      switch (ch) {
-      case 72: {
-	if (d.Y > 1)
-	{ d.Y -= 2; value -= MX; }
-	break;
+      infur.open("grid.dat", ios::binary | ios::in);infur.seekg(0);
+//furs *pt=NULL;
+
+      if (infur.is_open()) { // TODO figure out this shit
+        infur.read(reinterpret_cast<char *>(grid),sizeof(grid));//grid
+        drawbool(grid);
+        infur.read(reinterpret_cast<char *>(&cnt),sizeof(unsigned short));
+        //count done
+        tcnt=cnt;
+        furs *pt=new furs();
+        while(tcnt--)
+        {
+        infur.read(reinterpret_cast<char *>(&pt),sizeof(furs));
+        infur.read(reinterpret_cast<char *>(&cnt),sizeof(cnt));
+        ptr=new listfurs(pt->getcur(),cnt,pt->getcolor());
       }
-
-      case 80: {
-	if (d.Y < 19)
-	{ d.Y += 2; value += MX; }
-	break;
+        infur.close();
       }
+      else {clearlist({45,0},20);
+        gotoxy({ 45, 3 });
+  cout << "Unable to open file" << endl;
+  gotoxy({ 45, 4 });
+	cout << "Press <Enter>" << endl;
 
-      case 75: {
-	if (d.X > 1)
-	{  d.X -= 2; value -= 1; }
-	break;
+  cin.ignore();
       }
+      quit = false;
+      bckToMenu = false;
+      break;
+    }
 
-      case 77: {
-	if (d.X < 39)
-	{ d.X += 2; value += 1; }
-	break;
+    case 2:{
+    ifstream  fin("grid.dat",ios::in);
+    if(fin.is_open()){
+      fin.close();
+        remove("grid.dat");
       }
+       bckToMenu = true; quit = false;} break;
 
-      case 13: {
-	bool& t = grid[value / MX][value % MX];
+    case 3:
+      quit = true; bckToMenu = true;
+  }
 
-	if (!t) ptr = new listfurs(d, value);
-	else
-	{
-	  if (ptr == getfur(value))
-	  { if (!ptr->getp()) ptr = NULL;
-	    else ptr = ptr->getp(); }
-	  delete getfur(value);
-	}
-	producelist({ 45, 1 }, ptr);
-	gotoxy(d);
-	t ^= 1;
+  gchanged=false;
+    if (opt==0||!infur.is_open()){
+          for (register int i = 0; i < MY; i++)
+      for (register int j = 0; j < MX; j++)
+        grid[i][j] = 0;}
 
-	if (t) cout << char(177);
-	else cout << " ";
-	break;
-      }
+    if (!quit) {
+      if(!bckToMenu){
 
-      case 's': {
-	// #TODO:0 do save function
-	// TODO complete furs files
-	break;
-      }
-
-      case 98: {
-	// HACK quick exit
-	// IDEA make it replace fur linker list with menu,the grid should
-	// sustain and draw furs according to option
-
-	goto hi;
-      }
-      }
+    drawbool(grid);
+      producelist({ 45, 1 }, ptr);}
+    }
+  else break;
+  d = { 1, 1 };
       gotoxy(d);
+      value=0;
+
+    while (!bckToMenu)
+    {
+      if ((ch = hitkey()))
+      {
+  switch (ch) {
+  // movement
+  case 72: {
+    if (d.Y > 1)
+    { d.Y -= 2; value -= MX; }
+    break;
+  }
+
+  case 80: {
+    if (d.Y < (2 * MY - 1))
+    { d.Y += 2; value += MX; }
+    break;
+  }
+
+  case 75: {
+    if (d.X > 1)
+    {  d.X -= 2; value -= 1; }
+    break;
+  }
+
+  case 77: {
+    if (d.X < (2 * MX - 1))
+    { d.X += 2; value += 1; }
+    break;
+  }
+
+
+  case 13: { saved = false;gchanged=true;
+       bool& t = grid[value / MX][value % MX];
+
+       if (!t) ptr = new listfurs(d, value,0);
+       else
+       {
+         if (ptr == &getfur(value)) {
+           ptr = ptr->getp();
+         }
+         delete &getfur(value);
+       }
+       producelist({ 45, 1 }, ptr);
+       gotoxy(d);
+       t ^= 1;
+
+      drawbool(grid);
+
+       break; }
+
+  case 98: {
+    bckToMenu = true;
+    char x;
+    if (saved)
+    { deleteall();ptr=NULL; break; }
+    else
+    {
+      clearlist({ 45, 0 }, 20);
+      gotoxy({ 45, 3 });cout << "Save changes?(y/n)";
+      gotoxy({ 45, 4 });cout << "Press <Enter> to confirm";
+      gotoxy({ 45, 5 });cout << ">>";
+
+      cin >> x;
+      if (x!='n' && x!='y') {x='n';}
+      if(x == 'n'||x=='N') { deleteall(); ptr=NULL ;break; }
     }
   }
-hi:
-  deleteall();
+
+  case 's': {if(gchanged&&ptr){
+        ofstream outfur("grid.dat", ios::binary | ios::out | ios::trunc);
+
+        if (outfur.is_open()) {
+          listfurs *t=NULL,*temp = ptr;
+          furs     *pt   = temp->getf();
+          outfur.write(reinterpret_cast<char *>(grid), sizeof(grid));//grid
+          unsigned short sp=temp->getcount();
+          outfur.write(reinterpret_cast<char *>(&sp),sizeof(sp));//count of list
+    while(t!=ptr){
+      temp=ptr;
+          while (temp->getp()!=t)
+          {
+            temp = temp->getp();
+          }
+          pt = temp->getf();
+          outfur.write(reinterpret_cast<char *>(pt), sizeof(furs));//fur
+          sp=temp->Getgridval();
+          outfur.write(reinterpret_cast<char *>(&sp),sizeof(sp));//gridval
+          t=temp;
+        }
+          outfur.close();
+        }
+        saved = true;
+        if (bckToMenu) deleteall();
+       gchanged=false;} break;
+   }
+  }
+  gotoxy(d);
+      }
+
+    }
+  } while (!quit);
   return 0;
 }
